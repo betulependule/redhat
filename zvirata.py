cd1f814360789fa478e2a2c04f07cc3de637d960
quack <you@example.com>
2017-11-12 03:51:20 +0100

    pridan soubor zvirata.py a upraveny soubor 20171204
    protoze to tak ma byt

---
+++ zvirata.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+zvirata = {"jelen":4, "pavouk":8, "husa":2, "kralik":4, "krava":4, "zajic":4, "had":0,"clovek":2, "aligator":4, "orel":2}
+ctyrnozi = []
+
+for key, value in zvirata.items():
+	if value>=4:
+		ctyrnozi.append(key)
+	else:
+		pass
+print("Mezi crytnoha zvirata patri: {}".format(ctyrnozi))
+		
+lepsi_zvirata = {"jelen":{"srst":"chlupy", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}, "pavouk":{"srst":"chlupy", "pocet_nohou":"0", "pocet_oci":"8", "ocas":"ne"}, "husa":{"srst":"peri", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}, "kralik":{"srst":"chlupy", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}, "krava":{"srst":"chlupy", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}, "zajic":{"srst":"chlupy", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}, "had":{"srst":"supiny", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}, "clovek":{"srst":"chlupy??", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"bohuzel_ne"}, "aligator":{"srst":"supiny", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}, "orel":{"srst":"peri", "pocet_nohou":"0", "pocet_oci":"2", "ocas":"ano"}}
+
+kolo=1
+
+def balik_nohou(zvirata, lepsi_zvirata, kolo):
+	for i in lepsi_zvirata.values():
+		for key, value in i.items():
+			if key=="pocet_nohou" and kolo==2:
+				i["pocet_nohou"]=zvirata["pavouk"]
+				kolo=kolo+1
+			elif key=="pocet_nohou" and (kolo==3 or kolo==8 or kolo==10):
+				i["pocet_nohou"]=zvirata["husa"]
+				kolo=kolo+1
+			elif key=="pocet_nohou" and kolo==7:
+				i["pocet_nohou"]=zvirata["had"]
+				kolo=kolo+1
+			elif key=="pocet_nohou":
+				i["pocet_nohou"]=zvirata["jelen"]
+				kolo=kolo+1
+			else:
+				pass
+	print(lepsi_zvirata)
+
+balik_nohou(zvirata, lepsi_zvirata, kolo)
+
